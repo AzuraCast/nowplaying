de53f543a977626846dab9a5ae427831e9aa0869
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-09-08 22:03:32 -0500

    Switch to simpler cleanup method.

--- src/Adapter/Icecast.php
+++ src/Adapter/Icecast.php
@@ -50,7 +50,8 @@ final class Icecast extends AdapterAbstract
             throw new Exception('Remote server returned empty response.');
         }
 
-        $return = $this->getCleanJson($payload);
+        $payload = str_replace( '"title": -', '"title": null', $payload);
+        $return = @json_decode($payload, true, 512, JSON_THROW_ON_ERROR);
 
         if (!$return || !isset($return['icestats']['source'])) {
             throw new Exception(sprintf('Invalid response: %s', $payload));
@@ -91,55 +92,6 @@ final class Icecast extends AdapterAbstract
             $npAggregate->merge($np);
         }
         return $npAggregate;
-    }
-
-    protected function getCleanJson(string $payload): array
-    {
-        $payload = str_replace( '"title": -', '"title": null', $payload);
-
-        /*
-         * JSON Fixer
-         * Courtesy of: https://stackoverflow.com/questions/13236819/how-to-fix-badly-formatted-json-in-php
-         */
-        $patterns = [
-            // Garbage removal
-            "/([\s:,\{}\[\]])\s*'([^:,\{}\[\]]*)'\s*([\s:,\{}\[\]])/", // Find any character except colons, commas, curly and square brackets surrounded or not by spaces preceded and followed by spaces, colons, commas, curly or square brackets...
-            '/([^\s:,\{}\[\]]*)\{([^\s:,\{}\[\]]*)/', // Find any left curly brackets surrounded or not by one or more of any character except spaces, colons, commas, curly and square brackets...
-            "/([^\s:,\{}\[\]]+)}/", // Find any right curly brackets preceded by one or more of any character except spaces, colons, commas, curly and square brackets...
-            "/(}),\s*/", // JSON.parse() doesn't allow trailing commas
-
-            // Reformatting
-            '/([^\s:,\{}\[\]]+\s*)*[^\s:,\{}\[\]]+/', // Find or not one or more of any character except spaces, colons, commas, curly and square brackets followed by one or more of any character except spaces, colons, commas, curly and square brackets...
-            '/["\']+([^"\':,\{}\[\]]*)["\']+/', // Find one or more of quotation marks or/and apostrophes surrounding any character except colons, commas, curly and square brackets...
-            '/(")([^\s:,\{}\[\]]+)(")(\s+([^\s:,\{}\[\]]+))/', // Find or not one or more of any character except spaces, colons, commas, curly and square brackets surrounded by quotation marks followed by one or more spaces and  one or more of any character except spaces, colons, commas, curly and square brackets...
-            "/(')([^\s:,\{}\[\]]+)(')(\s+([^\s:,\{}\[\]]+))/", // Find or not one or more of any character except spaces, colons, commas, curly and square brackets surrounded by apostrophes followed by one or more spaces and  one or more of any character except spaces, colons, commas, curly and square brackets...
-            '/(})(")/', // Find any right curly brackets followed by quotation marks...
-            '/,\s+(})/', // Find any comma followed by one or more spaces and a right curly bracket...
-            '/\s+/', // Find one or more spaces...
-            '/^\s+/', // Find one or more spaces at start of string...
-        ];
-
-        $replacements = [
-            // Garbage removal
-            '$1 "$2" $3', // ...and put quotation marks surrounded by spaces between them;
-            '$1 { $2', // ...and put spaces between them;
-            '$1 }', // ...and put a space between them;
-            '$1', // ...so, remove trailing commas of any right curly brackets;
-
-            // Reformatting
-            '"$0"', // ...and put quotation marks surrounding them;
-            '"$1"', // ...and replace by single quotation marks;
-            '\\$1$2\\$3$4', // ...and add back slashes to its quotation marks;
-            '\\$1$2\\$3$4', // ...and add back slashes to its apostrophes;
-            '$1, $2', // ...and put a comma followed by a space character between them;
-            ' $1', // ...and replace by a space followed by a right curly bracket;
-            ' ', // ...and replace by one space;
-            '', //...and remove it.
-        ];
-
-        $cleaned = preg_replace($patterns, $replacements, $payload);
-
-        return @json_decode($cleaned, true, 512, JSON_THROW_ON_ERROR);
     }
 
     protected function getXmlNowPlaying(?string $mount = null): Result
